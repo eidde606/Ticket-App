656fdbd2f94613cf54b553e73b553c6c389e2cc7
eidde606 <eidde_606@yahoo.com>
2023-11-12 17:23:12 -0500

    Made changes to the error handling

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -7,9 +7,12 @@ import { useRouter } from "next/navigation";
 const DeleteBlock = ({ id }) => {
   const router = useRouter();
   const deleteTicket = async () => {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
-      method: "DELETE",
-    });
+    const res = await fetch(
+      `https://ticket-appapi-c28d792b52fd.herokuapp.com/api/Tickets/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
 
     if (res.ok) {
       router.refresh();
